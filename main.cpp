28d3f1b7389cb2f05c06246fd70a04f0a55ef903
nickmachnik <nick.machnik@gmail.com>
2022-06-14 16:22:00 +0200

    prototype of kendall call

--- main.cpp
+++ main.cpp
@@ -37,9 +37,24 @@ void call_skeleton()
     std::array<double, 4> C = {1.0, 0.4, 0.4, 1.0};
     std::array<double, NUMBER_OF_LEVELS> Th = threshold_array(n, alpha);
     int sepset[sepset_size];
-    memset(sepset, 0, sizeof sepset);
+    memset(sepset, 0, sizeof(sepset));
     int l = 0;
     Skeleton(C.data(), &p, G.data(), Th.data(), &l, &max_level, pmax.data(), sepset);
+}
+
+void call_cu_corr()
+{
+    const size_t num_markers = 3;
+    const size_t num_individuals = 10;
+    // this should be col-major
+    const float a[num_individuals * num_markers] = {0., 0., 1., 1., 1., 2., 2., 2., 0., 0.,
+                                                    1., 0., 0., 1., 1., 0., 0., 0., 1., 1.,
+                                                    0., 2., 2., 1., 0., 0., 2., 0., 1., 0.};
+    const size_t corr_matrix_size = num_markers * (num_markers - 1) / 2;
+    float marker_corr[corr_matrix_size];
+    memset(marker_corr, 0.0, sizeof(marker_corr));
+
+    // TODO: insert actuall call
 }
 
 auto main(int argc, char const *argv[]) -> int
