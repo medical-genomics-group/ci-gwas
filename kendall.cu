d9bd3bc97e821ce95be00dc38a5947fdba135cd2
nickmachnik <nick.machnik@gmail.com>
2022-06-15 14:56:50 +0200

    print h, use %z mod

--- kendall.cu
+++ kendall.cu
@@ -106,8 +106,8 @@ __global__ void cu_marker_corr_npn(const unsigned char *a, const size_t num_mark
         float kendall_corr = (concordant - discordant) / sqrt((concordant + discordant + ties_x) *
                                                               (concordant + discordant + ties_y));
 
-        printf("linear ix: %f, row: %u, col: %u, corr result: %f \n", lin_ix_f, row, col,
-               kendall_corr);
+        printf("linear ix: %f, row: %zu, col: %zu, h: %zu, corr result: %f \n", lin_ix_f, row, col,
+               h, kendall_corr);
 
         results[lin_ix] = sin(M_PI / 2 * kendall_corr);
     }
