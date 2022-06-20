b6dfd89a0fcfb7d9bc6cb6176d22c49f9a002617
nickmachnik <nick.machnik@gmail.com>
2022-06-20 13:57:09 +0200

    try %zu print for size_t

--- kendall.cu
+++ kendall.cu
@@ -173,10 +173,10 @@ __global__ void cu_bed_marker_corr_npn(const unsigned char *a, const size_t num_
     // should have ++ increment and adjust the start.
     for (size_t i = tix; i < col_len_bytes; i += NUMTHREADS) {
         // TODO: make sure that unpacking happens in correct order
-        printf("block [x: %f; y: %f] thread %d: unpacking byte at x: %d \n", row, col, tix,
+        printf("block [x: %f; y: %f] thread %d: unpacking byte at x: %zu \n", col, row, tix,
                col_start_x + i);
         unpack_bed_byte(a[col_start_x + i], bed_vals_x);
-        printf("block [x: %f; y: %f] thread %d: unpacking byte at y: %d \n", row, col, tix,
+        printf("block [x: %f; y: %f] thread %d: unpacking byte at y: %zu \n", col, row, tix,
                col_start_y + i);
         unpack_bed_byte(a[col_start_y + i], bed_vals_y);
 
