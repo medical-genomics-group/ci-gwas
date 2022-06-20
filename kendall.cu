3366fcfd805d7695468c81eb217948e5aa60d055
nickmachnik <nick.machnik@gmail.com>
2022-06-20 18:00:22 +0200

    refactor: rename a to x

--- kendall.cu
+++ kendall.cu
@@ -4,15 +4,15 @@
 #include "gpuerrors.h"
 #include "kendall.h"
 
-// This computes correlations under the assumption that the final
-// correlation matrix fits into the GPU RAM in one piece.
-// This is unrealistic, but will be good for benchmarking and testing.
+// Compute correlation matrix of all pairs of markers in 'a'.
+// The data is not partitioned.
+// 'a' is assumed to hold uncompressed genomic marker values without NaN.
 void cu_corr_npn(const unsigned char *a, const size_t num_markers, const size_t num_individuals,
                  float *results)
 {
     // This here assumes a non compressed a.
-    size_t a_bytes = num_markers * num_individuals * sizeof(unsigned char);
-    unsigned char *gpu_a;
+    size_t x_bytes = num_markers * num_individuals * sizeof(unsigned char);
+    unsigned char *gpu_x;
     float *gpu_results;
     int threads_per_block = NUMTHREADS;
     size_t output_length = num_markers * (num_markers - 1) / 2;
@@ -21,20 +21,20 @@ void cu_corr_npn(const unsigned char *a, const size_t num_markers, const size_t 
     // TODO: see if proper blocks give any performace increase
     int blocks_per_grid = output_length;
 
-    HANDLE_ERROR(cudaMalloc(&gpu_a, a_bytes));
-    HANDLE_ERROR(cudaMemcpy(gpu_a, a, a_bytes, cudaMemcpyHostToDevice));
+    HANDLE_ERROR(cudaMalloc(&gpu_x, x_bytes));
+    HANDLE_ERROR(cudaMemcpy(gpu_x, x, x_bytes, cudaMemcpyHostToDevice));
     HANDLE_ERROR(cudaMalloc(&gpu_results, output_bytes));
 
-    cu_marker_corr_npn<<<blocks_per_grid, threads_per_block>>>(gpu_a, num_markers, num_individuals,
+    cu_marker_corr_npn<<<blocks_per_grid, threads_per_block>>>(gpu_x, num_markers, num_individuals,
                                                                gpu_results);
 
     HANDLE_ERROR(cudaMemcpy(results, gpu_results, output_bytes, cudaMemcpyDeviceToHost));
-    HANDLE_ERROR(cudaFree(gpu_a));
+    HANDLE_ERROR(cudaFree(gpu_x));
     HANDLE_ERROR(cudaFree(gpu_results));
 }
 
 // A O(n) runtime Kendall implementation for uncompressed genomic marker data.
-__global__ void cu_marker_corr_npn(const unsigned char *a, const size_t num_markers,
+__global__ void cu_marker_corr_npn(const unsigned char *x, const size_t num_markers,
                                    const size_t num_individuals, float *results)
 {
     size_t tix = threadIdx.x;
@@ -49,16 +49,15 @@ __global__ void cu_marker_corr_npn(const unsigned char *a, const size_t num_mark
     float h = (-(row * row) + row * (2.0 * l + 1.0)) / 2.0;
     // offset of 1 because we don't compute the diagonal
     float col = lin_ix_f - h + row + 1;
-    size_t col_start_x = row * num_individuals;
-    size_t col_start_y = col * num_individuals;
+    size_t col_start_a = row * num_individuals;
+    size_t col_start_b = col * num_individuals;
 
     float thread_sum[9] = {0.0};
     __shared__ float thread_sums[NUMTHREADS][9];
-
     // TODO: it seems stupid to jump in memory, sequential reads are probably more efficient.
     // should have ++ increment and adjust the start.
     for (size_t i = tix; i < num_individuals; i += NUMTHREADS) {
-        thread_sum[(3 * a[col_start_x + i]) + a[col_start_y + i]] += 1.f;
+        thread_sum[(3 * x[col_start_a + i]) + x[col_start_b + i]] += 1.f;
     }
 
     for (size_t i = 0; i < 9; i++) {
@@ -98,13 +97,14 @@ __global__ void cu_marker_corr_npn(const unsigned char *a, const size_t num_mark
 
 // Kendall correlation computation for pairs of markers compressed in .bed format
 // without leading magic numbers.
-void cu_bed_corr_npn(const unsigned char *a, const size_t num_markers, const size_t num_individuals,
+// Correlation matrix is computed in one go, without any partitioning.
+void cu_bed_corr_npn(const unsigned char *x, const size_t num_markers, const size_t num_individuals,
                      float *results)
 {
     // this is ceil
     size_t col_len_bytes = (num_individuals + 3) / 4 * sizeof(unsigned char);
-    size_t a_bytes = col_len_bytes * num_markers;
-    unsigned char *gpu_a;
+    size_t x_bytes = col_len_bytes * num_markers;
+    unsigned char *gpu_x;
     float *gpu_results;
     int threads_per_block = NUMTHREADS;
     size_t output_length = num_markers * (num_markers - 1) / 2;
@@ -113,25 +113,23 @@ void cu_bed_corr_npn(const unsigned char *a, const size_t num_markers, const siz
     // TODO: see if proper blocks give any performace increase
     int blocks_per_grid = output_length;
 
-    HANDLE_ERROR(cudaMalloc(&gpu_a, a_bytes));
-    HANDLE_ERROR(cudaMemcpy(gpu_a, a, a_bytes, cudaMemcpyHostToDevice));
+    HANDLE_ERROR(cudaMalloc(&gpu_x, x_bytes));
+    HANDLE_ERROR(cudaMemcpy(gpu_x, x, x_bytes, cudaMemcpyHostToDevice));
     HANDLE_ERROR(cudaMalloc(&gpu_results, output_bytes));
 
-    printf("starting corr kernels \n");
-
     cu_bed_marker_corr_npn<<<blocks_per_grid, threads_per_block>>>(
-        gpu_a, num_markers, num_individuals, col_len_bytes, gpu_results);
+        gpu_x, num_markers, num_individuals, col_len_bytes, gpu_results);
     CudaCheckError();
 
     HANDLE_ERROR(cudaMemcpy(results, gpu_results, output_bytes, cudaMemcpyDeviceToHost));
-    HANDLE_ERROR(cudaFree(gpu_a));
+    HANDLE_ERROR(cudaFree(gpu_x));
     HANDLE_ERROR(cudaFree(gpu_results));
 }
 
 // A O(n) runtime Kendall implementation for compressed genomic marker data.
-// compression format is expected to be col-major .bed without NaN.
-// that means a should have dimensions ceil(num_individuals / 4) * num_markers
-__global__ void cu_bed_marker_corr_npn(const unsigned char *a, const size_t num_markers,
+// The compression format is expected to be col-major .bed without NaN
+// and without leading magic numbers.
+__global__ void cu_bed_marker_corr_npn(const unsigned char *x, const size_t num_markers,
                                        const size_t num_individuals, const size_t col_len_bytes,
                                        float *results)
 {
@@ -147,8 +145,8 @@ __global__ void cu_bed_marker_corr_npn(const unsigned char *a, const size_t num_
     float h = (-(row * row) + row * (2.0 * l + 1.0)) / 2.0;
     // offset of 1 because we don't compute the diagonal
     float col = lin_ix_f - h + row + 1;
-    size_t col_start_x = row * col_len_bytes;
-    size_t col_start_y = col * col_len_bytes;
+    size_t col_start_a = row * col_len_bytes;
+    size_t col_start_b = col * col_len_bytes;
 
     float thread_sum[9] = {0.0};
     __shared__ float thread_sums[NUMTHREADS][9];
@@ -156,13 +154,13 @@ __global__ void cu_bed_marker_corr_npn(const unsigned char *a, const size_t num_
     // TODO: it seems stupid to jump in memory, sequential reads are probably more efficient.
     // should have ++ increment and adjust the start.
     for (size_t i = tix; i < col_len_bytes; i += NUMTHREADS) {
-        size_t xix = 4 * (size_t)(a[col_start_x + i]);
-        size_t yix = 4 * (size_t)(a[col_start_y + i]);
+        size_t aix = 4 * (size_t)(x[col_start_a + i]);
+        size_t bix = 4 * (size_t)(x[col_start_b + i]);
         for (size_t j = 0; j < 4; j++) {
             if ((i * 4 + j) < num_individuals) {
-                float val_x = bed_lut_a[(xix + j)];
-                float val_y = bed_lut_a[(yix + j)];
-                size_t comp_ix = (size_t)((3 * val_x + val_y));
+                float val_a = bed_lut_a[(aix + j)];
+                float val_b = bed_lut_a[(bix + j)];
+                size_t comp_ix = (size_t)((3 * val_a + val_b));
                 thread_sum[comp_ix] += 1.f;
             }
         }
@@ -172,13 +170,9 @@ __global__ void cu_bed_marker_corr_npn(const unsigned char *a, const size_t num_
         thread_sums[tix][i] = thread_sum[i];
     }
 
-    // printf("block [x: %f; y: %f] thread %d: finished.", row, col, tix);
-
     // consolidate thread_sums
     __syncthreads();
     if (tix == 0) {
-        // printf("block [x: %f; y: %f]: making single sum", row, col);
-
         // produce single sum
         float s[9] = {0.0};
         for (size_t i = 0; i < NUMTHREADS; i++) {
