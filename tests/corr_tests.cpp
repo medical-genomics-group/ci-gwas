1ab4c40d2d54f365e5d36b52b42e434f9191c4db
nick machnik <nick.machnik@gmail.com>
2022-06-22 17:27:59 +0200

    impl cu_corr_npn test (not compiling)

--- tests/corr_tests.cpp
+++ tests/corr_tests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <test_data/bed_marker_test_set.h>
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
@@ -6,4 +7,39 @@ TEST(HelloTest, BasicAssertions) {
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(CuCorrNpnTest, ExpectedReturnVals) {
+    const size_t num_markers = BMT_NUM_MARKERS;
+    const size_t num_individuals = BMT_NUM_INDIVIDUALS;
+    const size_t num_phen = BMT_NUM_PHEN;
+    const size_t marker_cm_size = corr_matrix_size(num_markers);
+    const size_t marker_phen_cm_size = num_markers * num_phen;
+    const size_t phen_cm_size = corr_matrix_size(num_phen);
+    
+    float marker_corr[marker_cm_size];
+    memset(marker_corr, 0.0, sizeof(marker_corr));
+    float marker_phen_corr[marker_phen_cm_size];
+    memset(marker_phen_corr, 0.0, sizeof(marker_phen_corr));
+    float phen_corr[phen_cm_size];
+    memset(phen_corr, 0.0, sizeof(phen_corr));
+    
+    cu_corr_npn(bmt_marker_vals, bmt_phen_vals, num_markers, num_individuals, num_phen,
+                 bmt_marker_mean, bmt_marker_std, marker_corr, marker_phen_corr, phen_corr);
+    
+    float marker_corr_expected[marker_cm_size] = {0.299969, 0.924167, 0.0};
+    float marker_phen_corr_expected[marker_phen_cm_size] = {-0.982847, -0.856312, -0.380668, -0.405905, -0.695899, -0.928034};
+    float phen_corr_expected[phen_cm_size] = {0.808083};
+
+    for (size_t i = 0; i < marker_cm_size; i++) {
+        EXPECT_EQ(marker_corr[i], marker_corr_expected[i]);
+    }
+
+    for (size_t i = 0; i < marker_phen_cm_size; i++) {
+        EXPECT_EQ(marker_phen_corr[i], marker_phen_corr_expected[i]);
+    }
+
+    for (size_t i = 0; i < phen_cm_size; i++) {
+        EXPECT_EQ(phen_corr[i], phen_corr_expected[i]);
+    }   
 }
