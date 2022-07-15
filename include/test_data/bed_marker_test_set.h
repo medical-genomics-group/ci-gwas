43fa2290926b0e87c0997cf8e24326afa5d92fd3
nick machnik <nick.machnik@gmail.com>
2022-07-15 13:07:20 +0200

    fix: wrong means in test data

--- include/test_data/bed_marker_test_set.h
+++ include/test_data/bed_marker_test_set.h
@@ -20,7 +20,7 @@
 // ]
 // Column-major
 const unsigned char bmt_marker_vals[9] = {0x2a, 0xb0, 0x00, 0xcb, 0xb2, 0x0c, 0x2a, 0xba, 0x00};
-const float bmt_marker_mean[3] = {0.6, 1.1, 0.8};
+const float bmt_marker_mean[3] = {1.4, 0.9, 1.2};
 const float bmt_marker_std[3] = {0.66332496, 0.83066239, 0.6};
 
 // Column-major
