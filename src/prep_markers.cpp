fd8c7552e8454422912d1c8369ab697bb5da9d9c
nickmachnik <nick.machnik@gmail.com>
2022-08-29 13:09:32 +0200

    fix: clarify that we impute to mode, not median

--- src/prep_markers.cpp
+++ src/prep_markers.cpp
@@ -1,22 +1,23 @@
-#include <cmath>
-#include <iterator>
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <cassert>
-#include <mps/prep_markers.h>
 #include <mps/bed_lut.h>
 #include <mps/io.h>
+#include <mps/prep_markers.h>
+
+#include <cassert>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <vector>
 
 void split_bim_line(std::string line, std::string *buf)
 {
     std::istringstream ss(line);
- 
+
     std::string word;
     size_t word_ix = 0;
-    
+
     while (ss >> word)
     {
         assert((word_ix < 6) && "found more than 6 columns in bim file, aborting.");
@@ -25,7 +26,8 @@ void split_bim_line(std::string line, std::string *buf)
     }
 }
 
-auto parse_bim(std::string bim_path) -> bimInfo {
+auto parse_bim(std::string bim_path) -> bimInfo
+{
     bimInfo res;
     res.number_of_lines = 0;
     res.chr_ids = {};
@@ -35,9 +37,11 @@ auto parse_bim(std::string bim_path) -> bimInfo {
     std::string line;
     std::ifstream bim(bim_path);
 
-    while (std::getline(bim, line)) {
+    while (std::getline(bim, line))
+    {
         split_bim_line(line, bim_line);
-        if ((res.number_of_lines == 0) || (bim_line[0] != res.chr_ids.back())) {
+        if ((res.number_of_lines == 0) || (bim_line[0] != res.chr_ids.back()))
+        {
             res.chr_ids.push_back(bim_line[0]);
             res.num_markers_on_chr.push_back(0);
         }
@@ -48,32 +52,27 @@ auto parse_bim(std::string bim_path) -> bimInfo {
     return res;
 }
 
-// Split .bed by chromosome, impute NaN to median, compute col means and std
-void prep_bed(std::string bed_path,
-              std::string bim_path,
-              std::string fam_path,
-              std::string out_dir,
-              size_t mem_gb) // max memory used by this fn)
+// Split .bed by chromosome, impute NaN to mode, compute col means and std
+void prep_bed(std::string bed_path, std::string bim_path, std::string fam_path, std::string out_dir,
+              size_t mem_gb)  // max memory used by this fn)
 {
     size_t num_individuals = count_lines(fam_path);
-    bimInfo bim_info =  parse_bim(bim_path);
+    bimInfo bim_info = parse_bim(bim_path);
     size_t bed_block_size = (num_individuals + 3) / 4;
 
-    for (size_t i = 0; i < bim_info.chr_ids.size(); ++i) {
-        write_dims(
-                num_individuals,
-                bim_info.num_markers_on_chr[i],
-                out_dir,
-                bim_info.chr_ids[i]);
+    for (size_t i = 0; i < bim_info.chr_ids.size(); ++i)
+    {
+        write_dims(num_individuals, bim_info.num_markers_on_chr[i], out_dir, bim_info.chr_ids[i]);
     }
 
     std::vector<unsigned char> bed_block(bed_block_size);
     std::ifstream bed_file(bed_path, std::ios::binary);
 
     // read first three bytes, check if correct code
-    bed_file.read(reinterpret_cast<char*>(bed_block.data()), 3);
+    bed_file.read(reinterpret_cast<char *>(bed_block.data()), 3);
     unsigned char bed_col_maj_code[3] = {0x6c, 0x1b, 0x01};
-    for (size_t i = 0; i < 3; ++i) {
+    for (size_t i = 0; i < 3; ++i)
+    {
         assert((bed_block[i] == bed_col_maj_code[i]) && "unexpected magic number in bed file.");
     }
 
@@ -89,53 +88,67 @@ void prep_bed(std::string bed_path,
     size_t chr_ix = 0;
     size_t chr_processed_markers = 0;
 
-    while (bed_file.read(reinterpret_cast<char*>(bed_block.data()), bed_block_size)) {
-        // first pass: mean, median
+    while (bed_file.read(reinterpret_cast<char *>(bed_block.data()), bed_block_size))
+    {
+        // first pass: mean, mode
         gt_counts[0] = 0;
         gt_counts[1] = 0;
         gt_counts[2] = 0;
         size_t gt_sum = 0;
         size_t nan_count = 0;
 
-        for (size_t i = 0; i < bed_block_size; ++i) {
+        for (size_t i = 0; i < bed_block_size; ++i)
+        {
             unsigned char bed_byte = bed_block[i];
-            for (size_t j = 0; (j < 4) && (i * 4 + j < num_individuals); ++j) {
+            for (size_t j = 0; (j < 4) && (i * 4 + j < num_individuals); ++j)
+            {
                 size_t ix = (4 * bed_byte) + j;
-                if (bed_lut_b[ix] == 0.0) {
+                if (bed_lut_b[ix] == 0.0)
+                {
                     nan_count += 1;
-                } else {
+                }
+                else
+                {
                     gt_counts[(size_t)bed_lut_a[ix]] += 1;
                     gt_sum += bed_lut_a[ix];
                 }
             }
         }
-        
-        // find median
-        size_t median = 0;
-        for (size_t i = 1; i < 3; ++i) {
-            if (gt_counts[i] > gt_counts[median]) {
-                median = i;
+
+        // find mode
+        size_t mode = 0;
+        for (size_t i = 1; i < 3; ++i)
+        {
+            if (gt_counts[i] > gt_counts[mode])
+            {
+                mode = i;
             }
         }
 
-        gt_sum += (float)(median * nan_count);
+        gt_sum += (float)(mode * nan_count);
         float mean = gt_sum / (float)num_individuals;
 
         // second pass: impute, std
         float sum_squares = 0.0;
-        for (size_t i = 0; i < bed_block_size; ++i) {
+        for (size_t i = 0; i < bed_block_size; ++i)
+        {
             unsigned char new_byte = 0;
             unsigned char bed_byte = bed_block[i];
-            for (size_t j = 0; j < 4; ++j) {
+            for (size_t j = 0; j < 4; ++j)
+            {
                 new_byte <<= 2;
                 size_t ix = (4 * bed_byte) + 3 - j;
                 size_t curr_val = 2;
-                if ((i * 4 + 3 - j) < num_individuals) {
-                    if (bed_lut_b[ix] == 0) {
-                        curr_val = median;
-                    } else {
+                if ((i * 4 + 3 - j) < num_individuals)
+                {
+                    if (bed_lut_b[ix] == 0)
+                    {
+                        curr_val = mode;
+                    }
+                    else
+                    {
                         curr_val = (size_t)bed_lut_a[ix];
-                    } 
+                    }
                     float curr_diff = (float)curr_val - mean;
                     sum_squares += curr_diff * curr_diff;
                 }
@@ -150,9 +163,8 @@ void prep_bed(std::string bed_path,
         chr_processed_markers += 1;
 
         // write to file
-        if (
-                (blocks_in_buf >= max_num_blocks) || 
-                (chr_processed_markers == bim_info.num_markers_on_chr[chr_ix])) 
+        if ((blocks_in_buf >= max_num_blocks) ||
+            (chr_processed_markers == bim_info.num_markers_on_chr[chr_ix]))
         {
             write_bed(out_buf, out_dir, bim_info.chr_ids[chr_ix]);
             write_means(chr_marker_means, out_dir, bim_info.chr_ids[chr_ix]);
@@ -162,12 +174,11 @@ void prep_bed(std::string bed_path,
             // TODO: add magic numbers (maybe)
             out_buf.clear();
             blocks_in_buf = 0;
-            if (chr_processed_markers == bim_info.num_markers_on_chr[chr_ix]) {
+            if (chr_processed_markers == bim_info.num_markers_on_chr[chr_ix])
+            {
                 chr_ix += 1;
                 chr_processed_markers = 0;
             }
         }
     }
 }
-
-
