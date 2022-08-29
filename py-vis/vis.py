67ceacf8100a474698aa2c73c757f69143b8e6bd
nickmachnik <nick.machnik@gmail.com>
2022-08-29 11:07:41 +0200

    add optonal title to plot

--- py-vis/vis.py
+++ py-vis/vis.py
@@ -2,6 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import struct
 
+SMALL_SIZE = 12
+MEDIUM_SIZE = 14
+BIGGER_SIZE = 16
+
+plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
 
 def read_floats_from_bin(path: str, num_values: int) -> np.array:
     with open(path, "rb") as fin:
@@ -12,7 +24,7 @@ def corr_plot(
     b1: str,
     b2: str,
     num_markers: int,
-    # outpath: str
+    title="",
 ):
     num_values = int(num_markers * (num_markers - 1) / 2)
     v1 = read_floats_from_bin(b1, num_values)
@@ -36,5 +48,6 @@ def corr_plot(
         verticalalignment="top",
         bbox=props,
     )
+    if title:
+        plt.title(title)
     plt.tight_layout()
-    # plt.savefig(outpath)
