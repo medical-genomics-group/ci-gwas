a34845343807492aa076323a47642444c9753700
nick machnik <nick.machnik@gmail.com>
2022-09-23 14:39:57 +0200

    fix: Phen inits

--- src/phen.cpp
+++ src/phen.cpp
@@ -12,26 +12,36 @@ auto split_phen_line(std::string line) -> std::vector<float>
     {
         res.push_back((float)atof(full_line[i].c_str()));
     }
-    res;
+    return res;
 }
 
 auto load_phen(std::string phen_path) -> Phen
 {
     Phen res;
+    res.num_samples = 0;
+    res.num_phen = 0;
     std::string line;
     std::ifstream phen(phen_path);
     std::vector<float> row_major_vals;
+    bool firstline = true;
 
     while (std::getline(phen, line))
     {
         std::vector<float> phen_vals = split_phen_line(line);
-        if (res.num_phen == 0)
+        if (firstline)
         {
             res.num_phen = phen_vals.size();
+            firstline = false;
         }
         else if (res.num_phen != phen_vals.size())
         {
-            printf("Inconsistent row width in .phen file");
+            printf("Inconsistent row width in .phen file \n");
+            printf("Line: %s \n", line.c_str());
+            for (float v : phen_vals)
+            {
+                printf("%f \t", v);
+            }
+            printf("\n num_phen: %u \n", res.num_phen);
             exit(1);
         }
         for (float v : phen_vals)
@@ -48,5 +58,6 @@ auto load_phen(std::string phen_path) -> Phen
             res.data.push_back(row_major_vals[(res.num_phen * i) + p]);
         }
     }
+
     return res;
 }
