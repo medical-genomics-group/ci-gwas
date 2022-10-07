72db93e422d399b9cd797433afdacd6ac872ba47
nickmachnik <nick.machnik@gmail.com>
2022-10-07 16:08:53 +0200

    impl cupc indpl0 test

--- tests/cupc_tests.cpp
+++ tests/cupc_tests.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <gtest/gtest.h>
 #include <mps/cu_test_helpers.h>
-#include <test_data/bed_marker_test_set.h>
+#include <test_data/cupc_test_set.h>
 #include <vector>
 
 const int NUMBER_OF_LEVELS = 50;
@@ -28,10 +28,10 @@ auto threshold_array(const int n, const double alpha) -> std::array<double, NUMB
 
 TEST(cuPCSparseTests, CalIndepL1SingleBlock)
 {
-    const double alpha = 0.05;
-    int w = 3;
-    int p = BMT2_NUM_PHEN;
-    int m = BMT2_NUM_MARKERS;
+    const double alpha = 0.1;
+    int w = CUPCT1_W;
+    int p = CUPCT1_P;
+    int m = CUPCT1_M;
     int max_marker_degree = 2 * w + p;
     int max_phen_degree = m + p;
     size_t mixed_matrix_size = max_marker_degree * m + max_phen_degree * p;
@@ -39,17 +39,15 @@ TEST(cuPCSparseTests, CalIndepL1SingleBlock)
     std::array<double, NUMBER_OF_LEVELS> Th = threshold_array(m + p, alpha);
 
     test_cal_Indepl0(
-        bmt2_sparse_corrs,
+        cupct1_c,
         &m,
         &p,
         &w,
         G.data(),
         Th.data());
 
-    for (auto e : Th)
+    for (size_t i = 0; i < 59; ++i)
     {
-        printf("%f \n", e);
+        ASSERT_EQ(G[i], cupct1_g[i]);
     }
-
-    ASSERT_EQ(1, 2);
 }
