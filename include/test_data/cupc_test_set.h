88b649053707253f626cacd05003016da8d2c4f5
nickmachnik <nick.machnik@gmail.com>
2023-02-28 14:36:08 +0100

    add docstring for corr matrix example

--- include/test_data/cupc_test_set.h
+++ include/test_data/cupc_test_set.h
@@ -4,6 +4,18 @@
 #define CUPCT1_P 3
 #define CUPCT1_W 2
 
+/** This is a row-major matrix with the following structure:
+ * {
+ * 0.47, 0.39, 0.74, 0.53, 0.62,
+ * 0.64, 0.51, 0.35, 0.4,  0.15,
+ * 0.80, 0.15, 0.98, 0.21, 0.94,
+ * 0.50, 0.00, 0.33, 0.49, 0.25, // here only one forward entry
+ * 0.00, 0.00, 0.28, 0.38, 0.24, // here no forward entries
+ * 0.00, 0.00, 0.00, 0.21, 0.06, // the last three lines are the phenotype corrs in the lower right corner
+ * 0.00, 0.00, 0.21, 0.00, 0.99,
+ * 0.00, 0.00, 0.06, 0.99, 0.00
+ * }
+ */
 const float cupct1_c[40] = {0.47, 0.39, 0.74, 0.53, 0.62, 0.64, 0.51, 0.35, 0.4, 0.15, 0.8,
                             0.15, 0.98, 0.21, 0.94, 0.5, 0., 0.33, 0.49, 0.25, 0., 0.,
                             0.28, 0.38, 0.24, 0., 0., 0., 0.21, 0.06, 0., 0., 0.21,
