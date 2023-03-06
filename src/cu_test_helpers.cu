cac55abc1cb8482191e40e1886aed8c35ba44844
nickmachnik <nick.machnik@gmail.com>
2023-03-06 17:07:05 +0100

    rm scan compact sparse test for now

--- src/cu_test_helpers.cu
+++ src/cu_test_helpers.cu
@@ -58,41 +58,41 @@ void test_cal_Indepl0(const float *C, const int *M, const int *P, const int *W, 
     CudaCheckError();
 }
 
-void test_cal_scan_compact(int *G_compact, const int *G, int *nprime, const int *M, const int *P, const int *W)
-{
-    int *G_cuda;
-    int *G_compact_cuda;
-    int *nprime_cuda;
+// void test_cal_scan_compact(int *G_compact, const int *G, int *nprime, const int *M, const int *P, const int *W)
+// {
+//     int *G_cuda;
+//     int *G_compact_cuda;
+//     int *nprime_cuda;
 
-    int nr = w + p;
-    int max_marker_degree = 2 * w + p;
-    int max_phen_degree = nr;
-    int adj_mat_size = max_marker_degree * m + max_phen_degree * p;
-    int compact_adj_mat_size = max_marker_degree * m + max_phen_degree * p + nr;
+//     int nr = w + p;
+//     int max_marker_degree = 2 * w + p;
+//     int max_phen_degree = nr;
+//     int adj_mat_size = max_marker_degree * m + max_phen_degree * p;
+//     int compact_adj_mat_size = max_marker_degree * m + max_phen_degree * p + nr;
 
-    // num phen
-    int p = *P;
-    // num markers
-    int m = *M;
-    // corr width
-    int w = *W;
+//     // num phen
+//     int p = *P;
+//     // num markers
+//     int m = *M;
+//     // corr width
+//     int w = *W;
 
-    HANDLE_ERROR(cudaMalloc((void **)&G_cuda, adj_mat_size * sizeof(int)));
-    HANDLE_ERROR(cudaMalloc((void **)&G_compact_cuda, compact_adj_mat_size * sizeof(int)));
-    HANDLE_ERROR(cudaMalloc((void **)&nprime_cuda, 1 * sizeof(int)));
-    HANDLE_ERROR(cudaMemcpy(G_cuda, G, adj_mat_size * sizeof(int), cudaMemcpyHostToDevice));
-    HANDLE_ERROR(cudaMemset(nprime_cuda, 0, 1 * sizeof(int)));
-    CudaCheckError();
+//     HANDLE_ERROR(cudaMalloc((void **)&G_cuda, adj_mat_size * sizeof(int)));
+//     HANDLE_ERROR(cudaMalloc((void **)&G_compact_cuda, compact_adj_mat_size * sizeof(int)));
+//     HANDLE_ERROR(cudaMalloc((void **)&nprime_cuda, 1 * sizeof(int)));
+//     HANDLE_ERROR(cudaMemcpy(G_cuda, G, adj_mat_size * sizeof(int), cudaMemcpyHostToDevice));
+//     HANDLE_ERROR(cudaMemset(nprime_cuda, 0, 1 * sizeof(int)));
+//     CudaCheckError();
 
-    BLOCKS_PER_GRID = dim3(1, nr, 1);
-    THREADS_PER_BLOCK = dim3(1024, 1, 1);
+//     BLOCKS_PER_GRID = dim3(1, nr, 1);
+//     THREADS_PER_BLOCK = dim3(1024, 1, 1);
 
-    scan_compact<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nr * sizeof(int)>>>(
-        GPrime_cuda, G_cuda, nr, nprime_cuda);
-    CudaCheckError();
+//     scan_compact<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nr * sizeof(int)>>>(
+//         GPrime_cuda, G_cuda, nr, nprime_cuda);
+//     CudaCheckError();
 
-    // Copy results back
-    HANDLE_ERROR(cudaMemcpy(nprime, nprime_cuda, 1 * sizeof(int), cudaMemcpyDeviceToHost));
-    HandleError(cudaMemcpy(G_compact, G_compact_cuda, compact_adj_mat_size * sizeof(int), cudaMemcpyDeviceToHost));
-    CudaCheckError();
-}
+//     // Copy results back
+//     HANDLE_ERROR(cudaMemcpy(nprime, nprime_cuda, 1 * sizeof(int), cudaMemcpyDeviceToHost));
+//     HandleError(cudaMemcpy(G_compact, G_compact_cuda, compact_adj_mat_size * sizeof(int), cudaMemcpyDeviceToHost));
+//     CudaCheckError();
+// }
