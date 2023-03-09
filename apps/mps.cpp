ee767393c0c9916563539c6793e1d03a1dc75df3
nickmachnik <nick.machnik@gmail.com>
2023-03-09 13:01:19 +0100

    put boost dependencies in own file

--- apps/mps.cpp
+++ apps/mps.cpp
@@ -1,42 +1,20 @@
-#include "mps/cuPC-S.h"
 #include <math.h>
 #include <mps/bim.h>
 #include <mps/corr_host.h>
 #include <mps/corr_kernels.h>
+#include <mps/cuPC-S.h>
+#include <mps/cuPC_call_prep.h>
 #include <mps/io.h>
 #include <mps/phen.h>
 #include <mps/prep.h>
 
 #include <array>
-#include <boost/math/distributions/normal.hpp>
 #include <cassert>
 #include <filesystem>
 #include <iostream>
 #include <string>
 #include <sys/stat.h>
 #include <vector>
-
-const int NUMBER_OF_LEVELS = 14;
-
-auto std_normal_qnorm(const double p) -> double
-{
-    return 1.0;
-    //boost::math::normal dist(0.0, 1.0);
-    //return quantile(dist, p);
-}
-
-auto threshold_array(const int n, const double alpha) -> std::array<double, NUMBER_OF_LEVELS>
-{
-    std::array<double, NUMBER_OF_LEVELS> thr{0.0};
-    // my loop range is exclusive of the last i, so I don't subtract one here
-    const int n_thr = (NUMBER_OF_LEVELS < (n - 3)) ? NUMBER_OF_LEVELS : n;
-    const double half = 0.5;
-    for (size_t i = 0; i < n_thr; i++)
-    {
-        thr[i] = abs(std_normal_qnorm(half * alpha) / sqrt(n - i - 3));
-    }
-    return thr;
-}
 
 /**
  * @brief Compute number of variables from upper triangular matrix (diag excluded)
---
+++ include/mps/cuPC_call_prep.h
@@ -0,0 +1,5 @@
+#pragma once
+
+double std_normal_qnorm(const double p);
+
+std::array<double, NUMBER_OF_LEVELS> threshold_array(const int n, const double alpha);
