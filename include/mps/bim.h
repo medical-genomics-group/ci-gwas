a2eef344e6ddec1eb5db7472225860bdb7fbf4e7
nickmachnik <nick.machnik@gmail.com>
2023-03-13 15:22:53 +0100

    add constructor for bimInfo

--- include/mps/bim.h
+++ include/mps/bim.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -9,6 +11,29 @@ struct bimInfo
     size_t number_of_lines;
     std::vector<std::string> chr_ids;
     std::vector<size_t> num_markers_on_chr;
+
+    bimInfo(std::string path)
+    {
+        number_of_lines = 0;
+        chr_ids = {};
+        num_markers_on_chr = {};
+
+        std::string bim_line[BIM_NUM_COLS];
+        std::string line;
+        std::ifstream bim(path);
+
+        while (std::getline(bim, line))
+        {
+            split_bim_line(line, bim_line);
+            if ((number_of_lines == 0) || (bim_line[0] != chr_ids.back()))
+            {
+                chr_ids.push_back(bim_line[0]);
+                num_markers_on_chr.push_back(0);
+            }
+            ++num_markers_on_chr.back();
+            ++number_of_lines;
+        }
+    }
 };
 
 void split_bim_line(std::string line, std::string *buf);
