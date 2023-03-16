c590c71e50e0f5b8dcebbb93b60bd5e5b6b2def7
nickmachnik <nick.machnik@gmail.com>
2023-03-16 15:07:27 +0100

    move BedDims back into io.h
    otherwise I have circular dependencies

--- include/mps/io.h
+++ include/mps/io.h
@@ -11,6 +11,50 @@ void split_line(std::string line, std::string *buf, size_t ncols);
 
 std::vector<std::string> split_line(std::string line);
 
+class BedDims
+{
+   private:
+    size_t num_samples;
+    size_t num_markers;
+
+   public:
+    BedDims(size_t num_samples, size_t num_markers)
+        : num_samples(num_samples), num_markers(num_markers)
+    {
+    }
+
+    BedDims(std::string path)
+    {
+        std::string dim_line[2];
+        std::string line;
+        std::ifstream dim_file(path);
+        std::getline(dim_file, line);
+        split_line(line, dim_line, 2);
+        num_samples = std::stoi(dim_line[0]);
+        num_markers = std::stoi(dim_line[1]);
+    }
+
+    BedDims(BfilesBase bfiles)
+    {
+        num_samples = count_lines(bfiles.fam());
+        num_markers = count_lines(bfiles.bim());
+    }
+
+    size_t get_num_markers() const { return num_markers; }
+
+    size_t get_num_samples() const { return num_samples; }
+
+    size_t bytes_per_col() const { return (num_samples + 3) / 4; }
+
+    void to_file(std::string path) const
+    {
+        std::ofstream fout;
+        fout.open(path, std::ios::out);
+        fout << num_samples << "\t" << num_markers << std::endl;
+        fout.close();
+    }
+};
+
 std::vector<float> read_floats_from_line_range(const std::string path, size_t first, size_t last);
 
 std::vector<unsigned char> read_block_from_bed(
