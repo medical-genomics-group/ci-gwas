b251c35d40943d4ca1fba9dd9dc47135daa2ed1a
nickmachnik <nick.machnik@gmail.com>
2023-03-16 15:14:56 +0100

    rm const qualifiers for map using fns

--- include/mps/bim.h
+++ include/mps/bim.h
@@ -47,7 +47,7 @@ struct BimInfo
         }
     }
 
-    void check_chr_id(std::string chr_id) const
+    void check_chr_id(const std::string chr_id) const
     {
         if (!chr_id2ix.contains(chr_id))
         {
@@ -56,20 +56,20 @@ struct BimInfo
         }
     }
 
-    size_t get_num_markers_on_chr(std::string chr_id) const
+    size_t get_num_markers_on_chr(const std::string chr_id)
     {
         check_chr_id(chr_id);
         return num_markers_on_chr[chr_id2ix[chr_id]];
     }
 
-    size_t get_global_chr_start(std::string chr_id) const
+    size_t get_global_chr_start(const std::string chr_id)
     {
         check_chr_id(chr_id);
         return global_chr_start[chr_id2ix[chr_id]];
     }
 
     // inclusive end
-    size_t get_global_chr_end(std::string chr_id) const
+    size_t get_global_chr_end(const std::string chr_id)
     {
         check_chr_id(chr_id);
         size_t chr_ix = chr_id2ix[chr_id];
