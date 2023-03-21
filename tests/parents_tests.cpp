9a894aee82508f4e7867c7bebd746a89f6559229
nickmachnik <nick.machnik@gmail.com>
2023-03-21 16:21:08 +0100

    add d0 test for parent test set

--- tests/parents_tests.cpp
+++ tests/parents_tests.cpp
@@ -4,6 +4,16 @@
 
 #include <vector>
 
+TEST(parent_set, ExpectedResultsDepth0)
+{
+    std::vector<int> obs =
+        parent_set(TEST_ADJ_MAT, TEST_NUM_MARKERS + TEST_NUM_PHEN, TEST_NUM_MARKERS, 0);
+    for (size_t i = 0; i < obs.size(); ++i)
+    {
+        EXPECT_EQ(TEST_PAR_SET_D0[i], obs[i]);
+    }
+}
+
 TEST(parent_set, ExpectedResultsDepth1)
 {
     std::vector<int> obs =
