0a7001735ee9988aa0b16701c2a66e07becebd1c
nickmachnik <nick.machnik@gmail.com>
2023-03-22 11:19:19 +0100

    make chr 1 test smaller

--- tests/blocking_tests.cpp
+++ tests/blocking_tests.cpp
@@ -36,40 +36,36 @@ TEST(block_chr, expected_results_synthetic_data)
     }
 }
 
-/*
 TEST(block_chr, expected_results_chr1)
 {
-    std::vector<MarkerBlock> obs = block_chr(CHR_ONE_MCORRK_SUM_ABS_ROWS, "1", 11000);
+    std::vector<MarkerBlock> obs = block_chr(CHR_ONE_MCORRK_SUM_ABS_ROWS, "1", 200);
     std::vector<MarkerBlock> exp = {
-        MarkerBlock("1", 0, 5090),        MarkerBlock("1", 5091, 11133),
-        MarkerBlock("1", 11134, 15892),   MarkerBlock("1", 15893, 22285),
-        MarkerBlock("1", 22286, 27105),   MarkerBlock("1", 27106, 30102),
-        MarkerBlock("1", 30103, 37499),   MarkerBlock("1", 37500, 42150),
-        MarkerBlock("1", 42151, 45358),   MarkerBlock("1", 45359, 49723),
-        MarkerBlock("1", 49724, 58066),   MarkerBlock("1", 58067, 63058),
-        MarkerBlock("1", 63059, 67599),   MarkerBlock("1", 67600, 75794),
-        MarkerBlock("1", 75795, 81929),   MarkerBlock("1", 81930, 87765),
-        MarkerBlock("1", 87766, 92475),   MarkerBlock("1", 92476, 95480),
-        MarkerBlock("1", 95481, 99014),   MarkerBlock("1", 99015, 102591),
-        MarkerBlock("1", 102592, 110967), MarkerBlock("1", 110968, 116784),
-        MarkerBlock("1", 116785, 123611), MarkerBlock("1", 123612, 129527),
-        MarkerBlock("1", 129528, 139781), MarkerBlock("1", 139782, 144870),
-        MarkerBlock("1", 144871, 151294), MarkerBlock("1", 151295, 157216),
-        MarkerBlock("1", 157217, 162050), MarkerBlock("1", 162051, 165281),
-        MarkerBlock("1", 165282, 169903)};
+        MarkerBlock("1", 0, 84),
+        MarkerBlock("1", 85, 151),
+        MarkerBlock("1", 152, 181),
+        MarkerBlock("1", 182, 283),
+        MarkerBlock("1", 284, 351),
+        MarkerBlock("1", 352, 403),
+        MarkerBlock("1", 404, 469),
+        MarkerBlock("1", 470, 563),
+        MarkerBlock("1", 564, 710),
+        MarkerBlock("1", 711, 787),
+        MarkerBlock("1", 788, 819),
+        MarkerBlock("1", 820, 903),
+        MarkerBlock("1", 904, 999)};
 
-    EXPECT_EQ(exp.size(), obs.size());
+    ASSERT_EQ(exp.size(), obs.size());
 
     for (size_t i = 0; i < exp.size(); ++i)
     {
         EXPECT_EQ(exp[i], obs[i]);
     }
 }
-*/
 
 TEST(hanning_smoothing, expected_results)
 {
-    std::vector<float> obs = hanning_smoothing(std::vector<float>(TEST_V.begin(), TEST_V.begin() + 1000), 101);
+    std::vector<float> obs =
+        hanning_smoothing(std::vector<float>(TEST_V.begin(), TEST_V.begin() + 1000), 101);
     std::vector<float> exp = TEST_V_SMOOTH;
 
     ASSERT_EQ(exp.size(), obs.size());
