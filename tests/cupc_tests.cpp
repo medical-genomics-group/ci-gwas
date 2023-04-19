f116543a8e52926bdf98bf564e71fb4808c189c3
nickmachnik <nick.machnik@gmail.com>
2023-04-19 21:35:19 +0200

    Replace sparse cuPC tests
    Sparse cuPC was not fully implemented and will most likely not be
    necessary after all, so that code is now removed, along with its tests
    and test data.
    In place of that we now have a unit test for cuPC for 10 variables.

--- tests/cupc_tests.cpp
+++ tests/cupc_tests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <mps/cuPC_call_prep.h>
 #include <mps/cu_test_helpers.h>
 #include <test_data/cupc_test_set.h>
 
@@ -6,40 +7,19 @@
 #include <cmath>
 #include <vector>
 
-const int NUMBER_OF_LEVELS = 50;
-
-auto std_normal_qnorm(const float p) -> float
+TEST(cuPCTests, ExpectedSkeletonN10)
 {
-    boost::math::normal dist(0.0, 1.0);
-    return quantile(dist, p);
-}
-
-auto threshold_array(const int n, const float alpha) -> std::array<float, NUMBER_OF_LEVELS>
-{
-    std::array<float, NUMBER_OF_LEVELS> thr{0.0};
-    // my loop range is exclusive of the last i, so I don't subtract one here
-    const int n_thr = (NUMBER_OF_LEVELS < (n - 3)) ? NUMBER_OF_LEVELS : n;
-    const float half = 0.5;
-    for (size_t i = 0; i < n_thr; i++)
-    {
-        thr[i] = abs(std_normal_qnorm(1 - half * alpha) / sqrt(n - i - 3));
-    }
-    return thr;
-}
-
-TEST(cuPCSparseTests, CalIndepL0SingleBlock)
-{
-    const float alpha = 0.1;
-    int w = CUPCT1_W;
-    int p = CUPCT1_P;
-    int m = CUPCT1_M;
-    int max_marker_degree = 2 * w + p;
-    int max_phen_degree = m + p;
-    size_t mixed_matrix_size = max_marker_degree * m + max_phen_degree * p;
-    std::vector<int> G(mixed_matrix_size, 0);
-    std::array<float, NUMBER_OF_LEVELS> Th = threshold_array(m + p, alpha);
-
-    test_cal_Indepl0(&cupct1_c[0], &m, &p, &w, G.data(), Th.data());
+    // call cuPC
+    int max_level = 14;
+    std::vector<float> Th = threshold_array(SAMPLE_SIZE_N10, ALPHA_N10);
+    int p = N_N10;
+    const size_t sepset_size = p * p * ML;
+    const size_t g_size = p * p;
+    std::vector<float> pmax(g_size, 0.0);
+    std::vector<int> G(g_size, 1);
+    std::vector<int> sepset(sepset_size, 0);
+    int l = 0;
+    Skeleton(C_N10.data(), &p, G.data(), Th.data(), &l, &max_level, pmax.data(), sepset.data());
 
     // printf("ix | obs | exp \n");
     // for (size_t i = 0; i < CUPCT1_ADJSIZE; ++i)
@@ -47,37 +27,8 @@ TEST(cuPCSparseTests, CalIndepL0SingleBlock)
     //     printf("%i | %i | %i \n", i, G[i], cupct1_g[i]);
     // }
 
-    for (size_t i = 0; i < CUPCT1_ADJSIZE; ++i)
+    for (size_t i = 0; i < p * p; ++i)
     {
-        EXPECT_EQ(G[i], cupct1_g[i]);
-    }
-}
-
-TEST(cuPCSparseTests, CalIndepL0MultiBlock)
-{
-    const float alpha = 0.1;
-    int w = CUPCT2_W;
-    int p = CUPCT2_P;
-    int m = CUPCT2_M;
-    int max_marker_degree = 2 * w + p;
-    int max_phen_degree = m + p;
-    size_t mixed_matrix_size = max_marker_degree * m + max_phen_degree * p;
-    std::vector<int> G(mixed_matrix_size, 0);
-    std::array<float, NUMBER_OF_LEVELS> Th = threshold_array(m + p, alpha);
-
-    test_cal_Indepl0(&cupct2_c[0], &m, &p, &w, G.data(), Th.data());
-
-    printf("ix | obs | exp \n");
-    for (size_t i = 0; i < CUPCT2_ADJSIZE; ++i)
-    {
-        if (G[i] != cupct2_g[i])
-        {
-            printf("%i | %i | %i \n", i, G[i], cupct2_g[i]);
-        }
-    }
-
-    for (size_t i = 0; i < CUPCT2_ADJSIZE; ++i)
-    {
-        EXPECT_EQ(G[i], cupct2_g[i]);
+        EXPECT_EQ(G[i], A_N10[i]);
     }
 }
