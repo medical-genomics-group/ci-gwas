d007849016164421c1259d229fc38414957be87b
nickmachnik <nick.machnik@gmail.com>
2023-05-05 17:09:16 +0200

    add tab10 cmap

--- scripts/bdpc.py
+++ scripts/bdpc.py
@@ -605,6 +605,19 @@ def plot_pag(pag_path: str, pheno_path: str, title=None, cmapix=0):
                 '#fddaec',
                 '#f2f2f2',
                 "#ffffff",  # white
+            ])),
+        mpl.colors.ListedColormap(
+            np.array([
+                '#1f77b4',
+                '#ff7f0e',
+                '#2ca02c',
+                '#d62728',
+                '#9467bd',
+                '#8c564b',
+                '#e377c2',
+                '#7f7f7f',
+                '#bcbd22',
+                "#ffffff",  # white
             ]))
     ]
     """
