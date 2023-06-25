fd7882e7b8f439705d112e73a9da30cf2bcdb23c
nickmachnik <nick.machnik@gmail.com>
2023-06-25 19:29:26 +0200

    fix errors
    - missing ;
    - wrong type of vec init

--- src/marker_summary_stats.cpp
+++ src/marker_summary_stats.cpp
@@ -5,8 +5,9 @@ MarkerSummaryStats::MarkerSummaryStats(const std::string path)
 {
     std::vector<float> triu_corrs = read_floats_from_binary(path);
     num_markers = ((sqrt(4 * 2 * triu_corrs.size()) - 1) / 2) + 1;
-    corrs = std::vector(num_markers * num_markers, 0.0);
-    size_t corr_ix = 0 for (size_t i = 0; i < num_markers; i++)
+    corrs = std::vector(num_markers * num_markers, (float)0.0);
+    size_t corr_ix = 0;
+    for (size_t i = 0; i < num_markers; i++)
     {
         for (size_t j = i + 1; j < num_markers; j++)
         {
