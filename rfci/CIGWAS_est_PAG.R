14ad461229d908af9005730c4c40c5c6e5378a8c
nick machnik <nick.machnik@gmail.com>
2023-08-04 16:54:16 +0200

    clean up PAG script

--- rfci/CIGWAS_est_PAG.R
+++ rfci/CIGWAS_est_PAG.R
@@ -1,5 +1,3 @@
-
-
 library("pcalg")
 library("Matrix");
 
@@ -7,22 +5,13 @@ source("./RFunctions/DAVS_funs.R")
 source("./RFunctions/rmdag6.R")
 source("./RFunctions/functions_rfci_davs.R")
 
-
 myargs = commandArgs(trailingOnly=TRUE)
-id= as.numeric(myargs[1])
-
-
-
-
-####################################
-
-#for e1 
-#MAX_LEVEL <- 5
-MAX_LEVEL <- 6
+input_filestem = myargs[2]
+num_individuals = myargs[3]
+output_file = myargs[4]
 
 form_sepset <- function(seps, nvar){
   res <- rep(list(rep(list(NULL), times=nvar)), nvar)
-  
   lapply(1:nvar, function(i)
     lapply(1:nvar, function(j) 
       lapply(1:MAX_LEVEL, function(k) {
@@ -55,95 +44,66 @@ loadSparseSepSetIntoNestedList = function(filepath, nvar) {
   return(res)
 }
 
-
-
-#for alpha=e8-e2
-stem = sprintf("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l6_a1e%s/all_merged",id)
-#stem = sprintf("/nfs/scistore13/robingrp/human_data/cigwas_estonia/bdpc_d1_l6_a1e%s_wo_bp/all_merged",id)
-#stem2 = sprintf("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l6_a1e%s/all_merged_replace",id)
-#for alpha=e1
-#stem = "/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l5_a1e1/all_merged"
-#for alpha=0.05
-#stem = "/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l6_a05/all_merged"
-
-
-
-mdim_path <- paste0(stem, ".mdim")
-sep_path <- paste0(stem, ".ssm")
+# number of variables, number of phenotypes, max_level
+mdim_path <- paste0(input_filestem, ".mdim")
+# separation set
+sep_path <- paste0(input_filestem, ".ssm")
 
 x <- read.csv(mdim_path, sep="\t", header=FALSE)
 num_var <- as.numeric(x[1])
 num_phen <- as.numeric(x[2])
-num_var
-num_phen
 sepset = loadSparseSepSetIntoNestedList(sep_path, num_var)
 
-#for alpha=e8-e2
-adjmat <- readMM(sprintf("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l6_a1e%s/all_merged_sam.mtx",id))
-cormat <- readMM(sprintf("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l6_a1e%s/all_merged_scm.mtx",id))
-#adjmat <- readMM(sprintf("/nfs/scistore13/robingrp/human_data/cigwas_estonia/bdpc_d1_l6_a1e%s_wo_bp/all_merged_sam.mtx",id))
-#cormat <- readMM(sprintf("/nfs/scistore13/robingrp/human_data/cigwas_estonia/bdpc_d1_l6_a1e%s_wo_bp/all_merged_scm.mtx",id))
-#for alpha=e1
+adjmat <- readMM(paste0(input_filestem, "_sam.mtx"))
+cormat <- readMM(paste0(input_filestem, "_scm.mtx"))
 
-#adjmat <- readMM("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l5_a1e1/all_merged_sam.mtx")
-#cormat <- readMM("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l5_a1e1/all_merged_scm.mtx")
-
-#for alpha=0.05
-
-#adjmat <- readMM("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l6_a05/all_merged_sam.mtx")
-#cormat <- readMM("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l6_a05/all_merged_scm.mtx")
-
-
-suffStat <- list(C=cormat ,n=458747)
-n=458747
+suffStat <- list(C=cormat, n=num_individuals)
 indepTest = gaussCItest
 alpha=0.05
-#p=dim(cormat)[1]
 
 conservative = FALSE 
 maj.rule = FALSE
 
-#Forcing PAG for marker-->phen and find unshielded triple for phen
-#adjmat[1:num_phen,num_phen+1:num_var][adjmat[1:num_phen,num_phen+1:num_var]==1]<-2
+print("Searching for unshielded triples")
+unsh_triple_pheno_only = FALSE
+if unsh_triple_pheno_only {
+    u.t <- find.unsh.triple(adjmat[1:num_phen,1:num_phen], check = FALSE)
+} else {
+    u.t <- find.unsh.triple(adjmat, check = FALSE)
+}
 
-print("PAG Started")
-u.t <- find.unsh.triple(adjmat[1:num_phen,1:num_phen], check = FALSE)
-
-#print("PAG Started")
-#u.t <- find.unsh.triple(adjmat, check = FALSE)
-print("rfci.vStruc Started")
-r.v. <- rfci.vStruc(suffStat = suffStat, indepTest = gaussCItest, alpha, sepset, adjmat, 
-                    unshTripl = u.t$unshTripl, unshVect = u.t$unshVect, conservative = (conservative || 
-                     maj.rule), version.unf = c(1, 1), maj.rule = maj.rule, 
-                   verbose = FALSE)
+print("Orienting v-structures")
+r.v. <- rfci.vStruc(suffStat=suffStat,
+                    indepTest=gaussCItest,
+                    alpha,
+                    sepset,
+                    adjmat, 
+                    unshTripl=u.t$unshTripl,
+                    unshVect=u.t$unshVect,
+                    conservative=(conservative || maj.rule),
+                    version.unf=c(1, 1),
+                    maj.rule = maj.rule,
+                    verbose = FALSE)
 A <- r.v.$amat
 sepset <- r.v.$sepset
-A[1:num_phen,(num_phen+1):num_var][A[1:num_phen,(num_phen+1):num_var]==1]<-3
-A[(num_phen+1):num_var,1:num_phen][A[(num_phen+1):num_var,1:num_phen]==1]<-2
-A
-print("udag2apag Started")
-res <- udag2apag(A[1:num_phen,1:num_phen], suffStat = suffStat, indepTest = gaussCItest, alpha, sepset, rules = rep(TRUE, 10), 
+force_marker_to_trait = FALSE
+if force_marker_to_trait {
+    A[1:num_phen,(num_phen+1):num_var][A[1:num_phen,(num_phen+1):num_var]==1]<-3
+    A[(num_phen+1):num_var,1:num_phen][A[(num_phen+1):num_var,1:num_phen]==1]<-2
+}
+
+print("Applying R1-R10")
+estimate_pag_with_traits_only = FALSE
+if estimate_pag_with_traits_only {
+    res <- udag2apag(A[1:num_phen,1:num_phen], suffStat = suffStat, indepTest = gaussCItest, alpha, sepset, rules = rep(TRUE, 10), 
                  unfVect = r.v.$unfTripl, verbose = FALSE)
+} else {
+    res <- udag2apag(A, suffStat = suffStat, indepTest = gaussCItest, alpha, sepset, rules = rep(TRUE, 10), 
+                 unfVect = r.v.$unfTripl, verbose = FALSE)
+}
 
 Amat <- res$graph
-
-Amat 
-
-#MMfile3 <- file.path(sprintf("/nfs/scistore13/robingrp/human_data/cigwas_estonia/bdpc_d1_l6_a1e%s_wo_bp",id), "all_merged_pag_mk3_ns_lut.mtx")
-MMfile3 <- file.path(sprintf("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l6_a1e%s",id),"pheno_pag_mk3_ns.mtx")
-#MMfile3                       
-#for alpha=e1
-#MMfile3 <- file.path("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l5_a1e1", "all_merged_pag_sk_mk3.mtx")
-#MMfile3 <- file.path("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l5_a1e1", "all_merged_pag_unsh_triple_mk3.mtx")
-
-#for alpha=0.05
-#MMfile3 <- file.path("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l6_a05", "all_merged_pag_sk_mk3.mtx")
-#MMfile3 <- file.path("/nfs/scistore13/robingrp/human_data/causality/parent_set_selection/production/bdpc_d1_l6_a05", "all_merged_pag_mk3_ns_lut.mtx")
+MMfile3 <- file.path(output_file)
 
 writeMM(Amat, file=MMfile3)
-print("Amat finished")
-
-
-
-#save(Amat,file = paste( "./CIGWAS_est_PAG/CIGWAS_est_PAG_lut_e",toString(id), ".RData", sep = "" ))
-######################################
+print("Done")
