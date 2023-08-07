62f56d30e18db9b6afeb12865ceecf71ec037505
nick machnik <nick.machnik@gmail.com>
2023-08-07 17:53:58 +0200

    clean up
    - remove unneeded imports
    - read max_level from .mdim file
    - take other params from command line

--- srfci/CIGWAS_est_PAG.R
+++ srfci/CIGWAS_est_PAG.R
@@ -1,21 +1,23 @@
-library("pcalg")
-library("Matrix");
+library("pcalg");
 
-source("./RFunctions/DAVS_funs.R")
-source("./RFunctions/rmdag6.R")
-source("./RFunctions/functions_rfci_davs.R")
+source("/nfs/scistore13/robingrp/nmachnik/dev/ci-gwas/srfci/RFCI_functions_final.R")
 
 myargs = commandArgs(trailingOnly=TRUE)
-input_filestem = myargs[2]
-num_individuals = myargs[3]
-output_file = myargs[4]
+print(myargs)
+input_filestem = myargs[1]
+num_individuals = as.numeric(myargs[2])
+output_file = myargs[3]
 
-form_sepset <- function(seps, nvar){
+print(paste0("input_filestem: ", input_filestem))
+print(paste0("num_individuals: ", num_individuals))
+print(paste0("output_file: ", output_file))
+
+form_sepset <- function(seps, nvar, max_level){
   res <- rep(list(rep(list(NULL), times=nvar)), nvar)
   lapply(1:nvar, function(i)
     lapply(1:nvar, function(j) 
-      lapply(1:MAX_LEVEL, function(k) {
-        val <- seps[ (((i - 1) * nvar + j) - 1) * MAX_LEVEL + k ]
+      lapply(1:max_level, function(k) {
+        val <- seps[ (((i - 1) * nvar + j) - 1) * max_level + k ]
         if (val != -1) {
           if (is.null(res[[i]][[j]])) {
             res[[i]][[j]] <<- c(val + 1)
@@ -52,6 +54,7 @@ sep_path <- paste0(input_filestem, ".ssm")
 x <- read.csv(mdim_path, sep="\t", header=FALSE)
 num_var <- as.numeric(x[1])
 num_phen <- as.numeric(x[2])
+max_level <- as.numeric(x[3])
 sepset = loadSparseSepSetIntoNestedList(sep_path, num_var)
 
 adjmat <- readMM(paste0(input_filestem, "_sam.mtx"))
@@ -66,7 +69,7 @@ maj.rule = FALSE
 
 print("Searching for unshielded triples")
 unsh_triple_pheno_only = FALSE
-if unsh_triple_pheno_only {
+if (unsh_triple_pheno_only) {
     u.t <- find.unsh.triple(adjmat[1:num_phen,1:num_phen], check = FALSE)
 } else {
     u.t <- find.unsh.triple(adjmat, check = FALSE)
@@ -82,19 +85,19 @@ r.v. <- rfci.vStruc(suffStat=suffStat,
                     unshVect=u.t$unshVect,
                     conservative=(conservative || maj.rule),
                     version.unf=c(1, 1),
-                    maj.rule = maj.rule,
-                    verbose = FALSE)
+                    maj.rule=maj.rule,
+                    verbose=TRUE)
 A <- r.v.$amat
 sepset <- r.v.$sepset
 force_marker_to_trait = FALSE
-if force_marker_to_trait {
+if (force_marker_to_trait) {
     A[1:num_phen,(num_phen+1):num_var][A[1:num_phen,(num_phen+1):num_var]==1]<-3
     A[(num_phen+1):num_var,1:num_phen][A[(num_phen+1):num_var,1:num_phen]==1]<-2
 }
 
-print("Applying R1-R10")
+print("Applying R5-R10")
 estimate_pag_with_traits_only = FALSE
-if estimate_pag_with_traits_only {
+if (estimate_pag_with_traits_only) {
     res <- udag2apag(A[1:num_phen,1:num_phen], suffStat = suffStat, indepTest = gaussCItest, alpha, sepset, rules = rep(TRUE, 10), 
                  unfVect = r.v.$unfTripl, verbose = FALSE)
 } else {
