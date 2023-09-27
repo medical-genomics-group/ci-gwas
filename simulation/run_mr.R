dc72208409ce999456f14fbe31d189e5fdf47858
nickmachnik <nick.machnik@gmail.com>
2023-09-27 15:55:25 +0200

    continue clean-up of mr run script

--- simulation/run_mr.R
+++ simulation/run_mr.R
@@ -1,3 +1,4 @@
+# do we really need all these packages?
 
 library("igraph");library("bnlearn")
 library("MRPRESSO");library("mixtools")
@@ -15,51 +16,90 @@ library("ff", quietly = TRUE)
 library("ivreg");library("mclust")
 library("tictoc")
 
-source("./MRfunctions.R")
+run_lm <- function(
+    x,
+    y,
+    z,
+    df
+) {
+    if(is.null(z)){
+        df = data.frame(x=df[,x],y=df[,y])
+    } else{
+        # this never happens, as we always specify z = NULL?
+        df = data.frame(x=df[,x],y=df[,y],df[,z])
+    }
+    model = lm(x~.,data=df)
+    coefs = summary(model)$coefficients
+    return(coefs[2,])
+}
 
-myargs = commandArgs(trailingOnly=TRUE)
-alpha_e = as.numeric(myargs[1])
-id = as.numeric(myargs[2])
+run_cause_on_tr <- function(
+    tr1,
+    phenos,
+    G_it,
+    GWAS_effects,
+    GWAS_ses
+) {
+    cause_res = c()
+    for(tr2 in phenos) {
+        if (tr1 == tr2){
+            next
+        }
+        if (is.null(G_it)) {
+            ivs = rownames(GWAS_effects)
+        } else {
+            ivs = rownames(G_it)[G_it[,tr1]]
+        }
+    
+        X = cbind(
+            GWAS_effects[ivs, tr1], GWAS_ses[ivs, tr1],
+            GWAS_effects[ivs, tr2], GWAS_ses[ivs, tr2]
+        )
+    
+        colnames(X) = c("beta_hat_1","seb1", "beta_hat_2","seb2")
+        X = data.frame(X)
+        X$snp = ivs
+        X = new_cause_data(X)
+        m_p = est_cause_params(X,variants = ivs)
+        m = cause(X,m_p)
+        # these p values are causal model better than null and causal better than shared.
+        # we don't use them though; is the p value in the summary the right one?
+        p1 = pnorm(m$elpd[2,5])
+        p2 = pnorm(m$elpd[3,5]) 
+        m_s = summary(m)
+        # @MR, if you could double check this, most importantly that m_s$p is the p-value
+        # for the causal model being better than the shared model..
+        # the tutorial is here: https://jean997.github.io/cause/ldl_cad.html
+        v = c(tr1, tr2, m_s$quants[[2]][1,1], m_s$p)
+        cause_res = rbind(cause_res, v)
+  }
+  
+  return(cause_res)
+}
 
-set.seed(id)
+
+args = commandArgs(trailingOnly=TRUE)
+indir = args[1]
+id = as.numeric(args[2])
 n=16000
 SNP=1600
+dag_data = readRDS(file.path(indir, paste("dag_data_n",  toString(n), "_SNP_", toString(SNP),"_it_", toString(id),".rds", sep = "")))
 
-
-load(file=paste( "./sim_small_effects/test_n", toString(n), "_SNP_", toString(SNP),"_Rep_",toString(id) , ".RData", sep = "" ))
-
-Tr=10
-#y_pa=5
-nL=2
-deg=3
-prob_pleio=0.2
-pq=SNP+Tr+nL
-
-Bg=myDAG3$G[(SNP+nL+1):pq,(SNP+nL+1):pq]
-Bg=t(Bg)
-Bg = igraph::graph_from_adjacency_matrix(t(abs(Bg)>0))
-#plot(igraph::simplify(Bg), vertex.color="green")
-B_distances = igraph_directed_distances(Bg)
-#print(B_distances)
-
-Y=myDAG3$x[,(SNP+nL+1):pq]
-###################################################################################
-# Create the input for MR
 all_mr_res = list()
 df = dat
-ivs = colnames(df)[grepl("X",colnames(df))]
-phenos = colnames(df)[grepl("Y",colnames(df))]
+ivs = colnames(df)[grepl("X", colnames(df))]
+phenos = colnames(df)[grepl("Y", colnames(df))]
 num_ivs = length(ivs)
 p = length(phenos)
 # Get all IV-phenotype associations
-GWAS_Ps = matrix(1,num_ivs,p, dimnames = list(ivs,phenos))
-GWAS_effects = matrix(0,num_ivs, p, dimnames = list(ivs,phenos))
-GWAS_ses = matrix(0,num_ivs, p, dimnames = list(ivs,phenos))
-GWAS_Zs = matrix(0,num_ivs, p, dimnames = list(ivs,phenos))
+GWAS_Ps = matrix(1, num_ivs, p, dimnames = list(ivs,phenos))
+GWAS_effects = matrix(0, num_ivs, p, dimnames = list(ivs,phenos))
+GWAS_ses = matrix(0, num_ivs, p, dimnames = list(ivs,phenos))
+GWAS_Zs = matrix(0, num_ivs, p, dimnames = list(ivs,phenos))
 
 for (pheno in phenos) {
     print(pheno)
-    gwas_res = sapply(ivs,run_lm,x=pheno,z=NULL,df = df)
+    gwas_res = sapply(ivs, run_lm, x=pheno, z=NULL, df=df)
     GWAS_Ps[,pheno] = gwas_res[4,]
     GWAS_effects[,pheno] = gwas_res[1,]
     GWAS_ses[,pheno] = gwas_res[2,]
@@ -69,23 +109,27 @@ for (pheno in phenos) {
 print(alpha_e)
 G_it = GWAS_Ps < alpha_e
 res_path="./mr_res_git_thr"
-MMfile_git <- file.path(res_path, paste("git_n", toString(n), "_SNP_", toString(SNP),  "_alpha_", toString(alpha_e), "_it_", toString(id), ".csv", sep = ""))
-print(MMfile_git)
-   print(G_it)
-  write.csv(G_it, file=MMfile_git,row.names = FALSE)
-  # Run MR
-  # pleio size is set to 100 - no filtering of variants (a standard MR analysis)
-  mr_anal_res = list(
+
+# this is needed only for tpr / fpr for the 
+# marker-trait links, but we don't do that anymore
+# MMfile_git <- file.path(res_path, paste("git_n", toString(n), "_SNP_", toString(SNP),  "_alpha_", toString(alpha_e), "_it_", toString(id), ".csv", sep = ""))
+# print(MMfile_git)
+# print(G_it)
+# write.csv(G_it, file=MMfile_git,row.names = FALSE)
+
+# Run MR
+# pleio size is set to 100 - no filtering of variants (a standard MR analysis)
+  mr_results = list(
     "Egger" = run_pairwise_mr_analyses(G_it,GWAS_effects,GWAS_ses,
                                        pleio_size=100,pruned_lists=NULL,func=mr_egger,robust=T),
     "IVW" = run_pairwise_mr_analyses(G_it,GWAS_effects,GWAS_ses,
                                      pleio_size=100,pruned_lists=NULL,func=mr_ivw,robust=T)
   )
-  print(mr_anal_res)
+  print(mr_results)
 
 
-  all_mr_res[["egger"]] = rbind(all_mr_res[["egger"]],mr_anal_res$Egger)
-  all_mr_res[["ivw"]] = rbind(all_mr_res[["ivw"]],mr_anal_res$IVW)
+  all_mr_res[["egger"]] = rbind(all_mr_res[["egger"]],mr_results$Egger)
+  all_mr_res[["ivw"]] = rbind(all_mr_res[["ivw"]],mr_results$IVW)
   
 MMfile1 <- file.path(res_path, paste("mr_skeleton_egger_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_it_",toString(id) , ".csv", sep = ""))##
 MMfile2 <- file.path(res_path, paste("mr_skeleton_ivw_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_it_",toString(id) , ".csv", sep = ""))##
@@ -132,19 +176,29 @@ write.csv(all_mr_res[2], file = MMfile2, row.names = FALSE)
   write.csv(all_mr_res[3], file = MMfile3, row.names = FALSE)
 
 
-  # Add CAUSE estimates - 
-   cause_res = mclapply(phenos,run_cause_on_tr,
-                        phenos=phenos,G_it=G_it,GWAS_effects=GWAS_effects,GWAS_ses=GWAS_ses,
-                        B_distances = B_distances,
-                        mc.cores = 4)
-   cause_res_all = c()
-   for(tr in cause_res){
-     if(length(tr)==1){next}
-     cause_res_all= rbind(cause_res_all,tr)
-   }
-   cause_res_all_df = data.frame(cause_res_all,stringsAsFactors = F)
-  for(j in 3:ncol(cause_res_all_df)){
-     cause_res_all_df[[j]] = as.numeric(as.character(cause_res_all_df[[j]]))
+# CAUSE
+cause_res = mclapply(
+    phenos,
+    run_cause_on_tr,
+    phenos=phenos,
+    G_it=G_it,
+    GWAS_effects=GWAS_effects,
+    GWAS_ses=GWAS_ses,
+    B_distances=B_distances,
+    mc.cores = 4)
+
+cause_res_all = c()
+for(tr in cause_res) {
+    if(length(tr)==1){
+        next
+    }
+    cause_res_all= rbind(cause_res_all, tr)
+}
+
+cause_res_all_df = data.frame(cause_res_all,stringsAsFactors = F)
+
+for(j in 3:ncol(cause_res_all_df)) {
+    cause_res_all_df[[j]] = as.numeric(as.character(cause_res_all_df[[j]]))
 }
  
 
