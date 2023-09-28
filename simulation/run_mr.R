247be8ef545f4f485dc0f284de3f67731c476f91
nickmachnik <nick.machnik@gmail.com>
2023-09-28 12:17:00 +0200

    finish cleaning mr script
    todo:
    add imported functions to script
    check that methods are run correctly
    test

--- simulation/run_mr.R
+++ simulation/run_mr.R
@@ -120,74 +120,83 @@ res_path="./mr_res_git_thr"
 # NM: why is pleio size 100 a standard MR analysis?
 # Run MR
 # pleio size is set to 100 - no filtering of variants (a standard MR analysis)
-mr_results = list(
-    "Egger"=run_pairwise_mr_analyses(
-        G_it,
-        GWAS_effects,
-        GWAS_ses,
-        pleio_size=100,
-        pruned_lists=NULL,
-        func=mr_egger,
-        robust=T),
-    "IVW"=run_pairwise_mr_analyses(
-        G_it,
-        GWAS_effects,
-        GWAS_ses,
-        pleio_size=100,
-        pruned_lists=NULL,
-        func=mr_ivw,
-        robust=T)
-  )
 
-all_mr_res[["egger"]] = rbind(all_mr_res[["egger"]], mr_results$Egger)
-all_mr_res[["ivw"]] = rbind(all_mr_res[["ivw"]], mr_results$IVW)
-  
-MMfile1 <- file.path(res_path, paste("mr_skeleton_egger_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_it_",toString(id) , ".csv", sep = ""))##
-MMfile2 <- file.path(res_path, paste("mr_skeleton_ivw_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_it_",toString(id) , ".csv", sep = ""))##
-write.csv(all_mr_res[1], file = MMfile1, row.names = FALSE)
-write.csv(all_mr_res[2], file = MMfile2, row.names = FALSE)
-  # Add MRPRESSO
-  mrpresso_res = c()
-  #cgauge_mrpresso_res = c()
-  try({
-    for(tr1 in phenos){
-      currivs = rownames(GWAS_Ps)[G_it[,tr1]]
-      for(tr2 in phenos){
-        if(tr1==tr2){next}
-        X = data.frame(E1b=GWAS_effects[currivs,tr1],O1b=GWAS_effects[currivs,tr2],
-                       E1sd=GWAS_ses[currivs,tr1],O1sd=GWAS_ses[currivs,tr2])
-        try({
-          res = mr_presso(BetaOutcome = "O1b", BetaExposure = "E1b", 
-                          SdOutcome = "O1sd", SdExposure = "E1sd",data=X,
-                          OUTLIERtest=T,
-                          DISTORTIONtest = T,
-                          NbDistribution = 1000,SignifThreshold = 0.1)
-          if(is.na(res$`Main MR results`[2,"P-value"])){
-            v = c(tr1,tr2,unlist(res$`Main MR results`[1,]))
-          }
-          else{
-            v = c(tr1,tr2,unlist(res$`Main MR results`[2,]))
-          }
-          v["GlobalTestP"] = res$`MR-PRESSO results`$`Global Test`$Pvalue
-          mrpresso_res = rbind(mrpresso_res,v)
-        })
-      }
+egger_res <- run_pairwise_mr_analyses(
+    G_it,
+    GWAS_effects,
+    GWAS_ses,
+    pleio_size=100,
+    pruned_lists=NULL,
+    func=mr_egger,
+    robust=T)
+
+write.csv(
+    egger_res,
+    file=file.path(res_path, paste("mr_egger_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_it_",toString(id) , ".csv", sep = "")),
+    row.names=FALSE)
+
+ivw_res <- run_pairwise_mr_analyses(
+    G_it,
+    GWAS_effects,
+    GWAS_ses,
+    pleio_size=100,
+    pruned_lists=NULL,
+    func=mr_ivw,
+    robust=T) 
+
+write.csv(
+    ivw_res,
+    file=file.path(res_path, paste("mr_ivw_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_it_",toString(id) , ".csv", sep = "")),
+    row.names=FALSE)
+
+mrpresso_res = c()
+# I don't know why these try blocks are here
+try({
+    for (tr1 in phenos) {
+        currivs = rownames(GWAS_Ps)[G_it[,tr1]]
+        for (tr2 in phenos) {
+            if (tr1 == tr2) {
+                next
+            }
+            X = data.frame(
+                    E1b=GWAS_effects[currivs,tr1],
+                    O1b=GWAS_effects[currivs,tr2],
+                    E1sd=GWAS_ses[currivs,tr1],
+                    O1sd=GWAS_ses[currivs,tr2])
+            try({
+                res = mr_presso(
+                    BetaOutcome="O1b",
+                    BetaExposure="E1b", 
+                    SdOutcome="O1sd",
+                    SdExposure="E1sd",
+                    data=X,
+                    OUTLIERtest=T,
+                    DISTORTIONtest=T,
+                    NbDistribution=1000,
+                    SignifThreshold=0.1)
+                if (is.na(res$`Main MR results`[2,"P-value"])) {
+                    v = c(tr1, tr2, unlist(res$`Main MR results`[1,]))
+                } else {
+                    v = c(tr1, tr2, unlist(res$`Main MR results`[2,]))
+                }
+                v["GlobalTestP"] = res$`MR-PRESSO results`$`Global Test`$Pvalue
+                mrpresso_res = rbind(mrpresso_res,v)
+            })
+        }
     }
-    if(!is.null(dim(mrpresso_res))){
-      mrpresso_res = as.data.frame(mrpresso_res)
-      for(j in 3:ncol(mrpresso_res)){
-        mrpresso_res[[j]] = as.numeric(as.character(mrpresso_res[[j]]))
-      }  
+    if (!is.null(dim(mrpresso_res))) {
+        mrpresso_res = as.data.frame(mrpresso_res)
+        for (j in 3:ncol(mrpresso_res)) {
+            mrpresso_res[[j]] = as.numeric(as.character(mrpresso_res[[j]]))
+        }
     }
-  })
-  print( mrpresso_res )
+})
 
-  all_mr_res[["mrpresso"]] = rbind(all_mr_res[["mrpresso"]],mrpresso_res)
-  MMfile3 <- file.path(res_path, paste("mr_skeleton_mrpresso_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_it_",toString(id) , ".csv", sep = ""))##
-  write.csv(all_mr_res[3], file = MMfile3, row.names = FALSE)
+write.csv(
+    mrpresso_res
+    file=file.path(res_path, paste("mr_mrpresso_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_it_",toString(id) , ".csv", sep = ""))),
+    row.names=FALSE)
 
-
-# CAUSE
 cause_res = mclapply(
     phenos,
     run_cause_on_tr,
@@ -195,34 +204,23 @@ cause_res = mclapply(
     G_it=G_it,
     GWAS_effects=GWAS_effects,
     GWAS_ses=GWAS_ses,
-    B_distances=B_distances,
-    mc.cores = 4)
+    mc.cores=4)
 
 cause_res_all = c()
-for(tr in cause_res) {
-    if(length(tr)==1){
+for (tr in cause_res) {
+    if (length(tr) == 1) {
         next
     }
-    cause_res_all= rbind(cause_res_all, tr)
+    cause_res_all = rbind(cause_res_all, tr)
 }
 
-cause_res_all_df = data.frame(cause_res_all,stringsAsFactors = F)
+cause_res_all_df = data.frame(cause_res_all, stringsAsFactors=F)
 
 for(j in 3:ncol(cause_res_all_df)) {
     cause_res_all_df[[j]] = as.numeric(as.character(cause_res_all_df[[j]]))
 }
  
-
-
-  
-  
-  
-all_mr_res[["CAUSE"]] = rbind(all_mr_res[["CAUSE"]],cause_res_all_df)
-
-save(all_mr_res, file=paste( "./mr_res_git_thr/all_mr_res_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_Rep_",toString(id) , ".RData", sep = "")) 
-
-res_path="./mr_res_git_thr"
-MMfile4 <- file.path(res_path, paste("mr_skeleton_cause_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_it_",toString(id) , ".csv", sep = ""))##
-#merged_all_mr <- bind_rows(all_mr_res [1], all_mr_res[2],all_mr_res[3],all_mr_res[4])
-
-write.csv(all_mr_res[4], file = MMfile4, row.names = FALSE)
+write.csv(
+    cause_res_all_df,
+    file=file.path(res_path, paste("mr_cause_n", toString(n), "_SNP_", toString(SNP),"_alpha_", toString(alpha_e),"_it_",toString(id) , ".csv", sep = "")),
+    row.names=FALSE)
