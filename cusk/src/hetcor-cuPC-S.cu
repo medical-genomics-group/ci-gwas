c4d3020ba6f3c417d6522399461ba68336f0fe04
Nick Noel Machnik <nick.machnik@gmail.com>
2024-05-29 16:21:36 +0200

    fix: was using Th as array

--- cusk/src/hetcor-cuPC-S.cu
+++ cusk/src/hetcor-cuPC-S.cu
@@ -78,7 +78,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(1, 1, 1);
                 THREADS_PER_BLOCK = dim3(32, 32, 1);
                 cal_Indepl0<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK>>>(
-                    C_cuda, G_cuda, Th[0], n
+                    C_cuda, G_cuda, Th, n
                 );
                 CudaCheckError();
             }
@@ -127,7 +127,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL1, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL1, 1, 1);
                 cal_Indepl1<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, Th[1], n
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, Th, n
                 );
                 CudaCheckError();
             }
@@ -138,7 +138,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL2, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL2, 1, 1);
                 cal_Indepl2<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[2]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -149,7 +149,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL3, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL3, 1, 1);
                 cal_Indepl3<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[3]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -160,7 +160,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL4, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL4, 1, 1);
                 cal_Indepl4<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[4]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -172,7 +172,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL5, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL5, 1, 1);
                 cal_Indepl5<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[5]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -184,7 +184,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL6, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL6, 1, 1);
                 cal_Indepl6<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[6]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
                 CudaCheckError();
@@ -196,7 +196,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL7, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL7, 1, 1);
                 cal_Indepl7<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[7]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -207,7 +207,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL8, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL8, 1, 1);
                 cal_Indepl8<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[8]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -218,7 +218,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL9, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL9, 1, 1);
                 cal_Indepl9<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[9]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -229,7 +229,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL10, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL10, 1, 1);
                 cal_Indepl10<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[10]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -240,7 +240,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL11, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL11, 1, 1);
                 cal_Indepl11<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[11]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -251,7 +251,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL12, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL12, 1, 1);
                 cal_Indepl12<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[12]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -262,7 +262,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL13, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL13, 1, 1);
                 cal_Indepl13<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[13]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
@@ -273,7 +273,7 @@ void hetcor_skeleton(
                 BLOCKS_PER_GRID = dim3(NumOfBlockForEachNodeL14, n, 1);
                 THREADS_PER_BLOCK = dim3(ParGivenL14, 1, 1);
                 cal_Indepl14<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK, nprime * sizeof(int)>>>(
-                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th[14]
+                    C_cuda, G_cuda, N_cuda, GPrime_cuda, mutex_cuda, n, Th
                 );
                 CudaCheckError();
             }
