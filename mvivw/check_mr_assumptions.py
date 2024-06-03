12f4261881ab8087ba0d92eaa031ac00fd5fc18b
nick machnik <nick.machnik@gmail.com>
2024-06-03 15:49:36 +0200

    impl mr assumption checks
    using only pearson corrs for now

--- mvivw/check_mr_assumptions.py
+++ mvivw/check_mr_assumptions.py
@@ -1,130 +1,83 @@
-## load mxp, pxp, corrs and ess, put into one matrix
-
+"""Fltering of exposures and instrument variables according to MR assumptions
 """
-All that we want to do here is to check MR assumptions:
-
-A) Avoid reverse causation
-    Filter out exposures for which we find a marker that satisfies
-        1) marker dep exposure | {}
-        2) marker indep exposure | outcome
-
-B) Valid IVs
-    Filter out markers which don't satisy
-        1) marker dep outcome | {}
-        2) marker indep outcome | exposures
-"""
-
+import numpy as np
+import pandas as pd
 from scipy.stats import norm
 from scipy.io import mmread
 
-def fisher_z(v):
+def _fisher_z(v):
     return np.abs(0.5 * np.log(np.abs((1 + v) / (1 - v))))
 
-def alpha_thr(alpha: int, n: int, l: int):
+def _alpha_thr(alpha: int, n: int, l: int):
     return norm.ppf(1 - (alpha / 2)) / np.sqrt(n - l - 3)
 
-def indep(x_ix, y_ix, S_ixs, corr, ess, alpha):
-    all_ixs = np.concatenate([[x_ix, y_ix], S_ixs]).astype(int)
-    ess_sub = ess[np.ix_(all_ixs, all_ixs)]
+def _indep(
+    x_ix: int,
+    y_ix: int,
+    s_ixs: list[int],
+    corr: np.array,
+    sample_size: int,
+    alpha: float
+):
+    all_ixs = np.concatenate([[x_ix, y_ix], s_ixs]).astype(int)
     corr_sub = corr[np.ix_(all_ixs, all_ixs)]
-    ess_mean = np.nanmean(ess_sub[np.triu_indices(len(all_ixs), 1)])
     m = np.linalg.inv(corr_sub)
-    pcorr = fisher_z(-(m[0, 1] / np.sqrt(np.abs(m[0, 0] * m[1, 1]))))
-    return pcorr < alpha_thr(alpha, ess_mean, len(S_ixs))
+    pcorr = _fisher_z(-(m[0, 1] / np.sqrt(np.abs(m[0, 0] * m[1, 1]))))
+    return pcorr < _alpha_thr(alpha, sample_size, len(s_ixs))
 
-def get_snp_parents(trait_ix, adj, num_traits):
+def _get_snp_parents(trait_ix, adj, num_traits):
     parents = np.where(adj[trait_ix, :])[0]
     return parents[parents > num_traits]
 
-wdir = "/nfs/scistore17/robingrp/nmachnik/mnt_home/projects/ci-gwas/mr_local_faithfulness"
-pxp_c_file = f"{wdir}/pxp_corrs.tsv"
-mxp_c_file = f"{wdir}/mxp_corrs.tsv"
-pxp_ess_file = f"{wdir}/pxp_ess.tsv"
-mxp_ess_file = f"{wdir}/mxp_ess.tsv"
+def _load_mdim(basepath: str):
+    with open(basepath + ".mdim", "r") as fin:
+        fields = fin.readline().strip().split("\t")
+    return [int(f) for f in fields]
 
-mxp_ess = pd.read_csv(mxp_ess_file, sep="\t")
-pxp_ess = pd.read_csv(pxp_ess_file, sep="\t")
-mxp_c = pd.read_csv(mxp_c_file, sep="\t")
-pxp_c = pd.read_csv(pxp_c_file, sep="\t")
+def check_ivs(result_basename: str, sample_size: int, alpha: float) -> pd.DataFrame:
+    # get correlation files
+    adj = mmread(f"{result_basename}_sam.mtx").toarray()
+    pearson_corr = mmread(f"{result_basename}_scm.mtx").toarray()
+    _, num_traits, _ = _load_mdim(result_basename)
+    # num_snp = num_var - num_traits
+    trait_ixs = np.arange(0, num_traits)
+    # snp_ixs = np.arange(num_traits, num_var)
+    iv_candidates = {trait_ix: set(_get_snp_parents(trait_ix, adj, num_traits)) for trait_ix in trait_ixs}
 
-n_traits = pxp_ess.shape[0]
-n_snps = mxp_ess.shape[0]
-n_var = n_traits + n_snps
-
-trait_ixs = np.arange(0, n_traits)
-snp_ixs = np.arange(n_traits, n_var)
-
-corr = np.zeros(shape=(n_var, n_var))
-ess = np.zeros(shape=(n_var, n_var))
-
-corr[np.ix_(trait_ixs, trait_ixs)] = pxp_c
-corr[np.ix_(trait_ixs, snp_ixs)] = mxp_c.T
-corr[np.ix_(snp_ixs, trait_ixs)] = mxp_c
-
-diag = np.arange(n_var)
-corr[diag, diag] = 1
-
-ess[np.ix_(trait_ixs, trait_ixs)] = pxp_ess
-ess[np.ix_(trait_ixs, snp_ixs)] = mxp_ess.T
-ess[np.ix_(snp_ixs, trait_ixs)] = mxp_ess
-
-trait_names = mxp_ess.columns
-
-adj = mmread("/nfs/scistore17/robingrp/nmachnik/mnt_home/projects/ci-gwas/ukb_8m/cuskss/all_alpha_e4/cuskss_merged_sam.mtx").toarray()
-pearson_corr = mmread("/nfs/scistore17/robingrp/nmachnik/mnt_home/projects/ci-gwas/ukb_8m/cuskss/all_alpha_e4/cuskss_merged_scm.mtx").toarray()
-full_ss_matrix = np.ones_like(ess) * 458747
-
-binary_traits = ["CAD", "AT", "T2D", "SMK", "ST"]
-binary_trait_ixs = [np.where(trait_names == trait_name)[0][0] for trait_name in binary_traits]
-iv_candidates = {trait_ix: set(get_snp_parents(trait_ix, adj, n_traits)) for trait_ix in trait_ixs}
-
-alpha = 0.0001
-
-# we re-evaluate the rparents for binary exposures,
-# because we found that there may be a high false positive rate in x -> y_cont -> y_bin situations when CI(x, y_bin | y_cont) is tested.
-
-binary_traits = ["CAD", "AT", "T2D", "SMK", "ST"]
-binary_trait_ixs = [np.where(trait_names == trait_name)[0][0] for trait_name in binary_traits]
-iv_candidates = {trait_ix: get_snp_parents(trait_ix, adj, n_traits) for trait_ix in trait_ixs}
-
-for outcome in trait_ixs:
-    for exposure in binary_trait_ixs:
-        if exposure == outcome:
-            continue
-        residual_ivs = set()
-        for snp in iv_candidates[exposure]:
-            if not indep(snp, exposure, [outcome], corr, ess, alpha):
-                residual_ivs.add(snp)
-        iv_candidates[exposure] = residual_ivs
-
-# where do we have evidence for reverse causation?
-# outcome -> exposures
-rev_cause = {trait_ix: set() for trait_ix in trait_ixs}
-for outcome in trait_ixs:
-    for exposure in trait_ixs:
-        if exposure == outcome:
-            continue
-        for snp in iv_candidates[outcome]:
-            if exposure in binary_trait_ixs:
-                marginal_dep = not indep(snp, exposure, [], corr, ess, alpha)
-                cond_indep = indep(snp, exposure, [outcome], corr, ess, alpha)
-            else:
-                marginal_dep = not indep(snp, exposure, [], pearson_corr, full_ss_matrix, alpha)
-                cond_indep = indep(snp, exposure, [outcome], pearson_corr, full_ss_matrix, alpha)
-            if marginal_dep and cond_indep:
-                rev_cause[outcome].add(exposure)
-
-trait_ixs_set = set(trait_ixs)
-valid_exposures = {trait_ix: trait_ixs_set - (rev_cause[trait_ix] | set([trait_ix])) for trait_ix in trait_ixs}
-
-# filter snps for valid ivs
-iv_snps = {(e, o): set() for e in trait_ixs for o in trait_ixs if e != o}
-for exposure in trait_ixs:
+    # where do we have evidence for reverse causation?
+    # outcome -> exposures
+    rev_cause = {trait_ix: set() for trait_ix in trait_ixs}
     for outcome in trait_ixs:
-        for snp in iv_candidates[exposure]:
-            if outcome in binary_trait_ixs:
-                cond_indep = indep(snp, outcome, list(valid_exposures[outcome]), corr, ess, alpha)
-            else:
-                cond_indep = indep(snp, outcome, list(valid_exposures[outcome]), pearson_corr, full_ss_matrix, alpha)
-            if cond_indep:
-                iv_snps[(exposure, outcome)].add(snp)
+        for exposure in trait_ixs:
+            if exposure == outcome:
+                continue
+            for snp in iv_candidates[outcome]:
+                marginal_dep = not _indep(snp, exposure, [], pearson_corr, sample_size, alpha)
+                cond_indep = _indep(snp, exposure, [outcome], pearson_corr, sample_size, alpha)
+                if marginal_dep and cond_indep:
+                    rev_cause[outcome].add(exposure)
+    
+    trait_ixs_set = set(trait_ixs)
+    valid_exposures = {trait_ix: trait_ixs_set - (rev_cause[trait_ix] | set([trait_ix])) for trait_ix in trait_ixs}
+
+    # filter snps for valid ivs
+    iv_snps = {(e, o): set() for e in trait_ixs for o in trait_ixs if e != o}
+    for outcome in trait_ixs:
+        for exposure in valid_exposures[outcome]:
+            for snp in iv_candidates[exposure]:
+                marginal_dep = not _indep(snp, outcome, [], pearson_corr, sample_size, alpha)
+                cond_indep = _indep(snp, outcome, list(valid_exposures[outcome]), pearson_corr, sample_size, alpha)
+                if cond_indep and marginal_dep:
+                    iv_snps[(exposure, outcome)].add(snp)
+    
+    rows = []
+    for (e_ix, o_ix), ivs in iv_snps.items():
+        for snp_ix in ivs:
+            # +1 for 1-based indexing in R
+            rows.append({
+                "Exposure": e_ix + 1,
+                "Outcome": o_ix + 1,
+                "IV": snp_ix + 1 - num_traits,
+            })
+    
+    return pd.DataFrame(rows)
