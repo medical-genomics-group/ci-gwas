c2f27d8527d911179fd0c1f3128a01cadc6a042d
nick machnik <nick.machnik@gmail.com>
2025-03-06 14:50:36 +0100

    fix: broken results when no markers for outcome

--- mvivw/cig_mvivw.R
+++ mvivw/cig_mvivw.R
@@ -39,16 +39,19 @@ SE = (1 - B * B) / sqrt(num_samples - 2)
 est_eff = matrix(0, num_trait, num_trait)
 est_p = matrix(1.0, num_trait, num_trait)
 
-# TODO: refactor, this is terrible
+# TODO: refactor
 sources = c()
 sinks = c()
 effects = c()
 ps = c()
 sk_adj = c()
+num_snp = c()
 
 for (outcome_ix in 1:num_trait) {
     # we drop the rows which are parents of the outcome
     outcome_eff_rows = which(mxp_adj[, outcome_ix] == 1)
+    rix = seq(1, dim(B)[1])
+    rix_keep = rix[! rix %in% outcome_eff_rows]
     if (rm_non_adjacent) {
         tested_traits = which(pxp_adj[, outcome_ix] == 1)
         # rm rows which are parents of the removed traits
@@ -73,16 +76,16 @@ for (outcome_ix in 1:num_trait) {
         byse = SE[-rm_rows, outcome_ix]
     } else {
         tested_traits = seq(1, num_trait)[-outcome_ix]
-        bx = B[-outcome_eff_rows, -outcome_ix, drop=FALSE]
-        bxse = SE[-outcome_eff_rows, -outcome_ix, drop=FALSE]
-        by = B[-outcome_eff_rows, outcome_ix]
-        byse = SE[-outcome_eff_rows, outcome_ix]
+        bx = B[rix_keep, -outcome_ix, drop=FALSE]
+        bxse = SE[rix_keep, -outcome_ix, drop=FALSE]
+        by = B[rix_keep, outcome_ix]
+        byse = SE[rix_keep, outcome_ix]
     }
     # make sure that we have more ivs than exposures
     sufficient_ivs = dim(bx)[1] > dim(bx)[2]
     if ((length(tested_traits) > 0) && (sufficient_ivs)) {
         if (use_ld) {
-            sub_ld_mat = full_ld_mat[-outcome_eff_rows, -outcome_eff_rows]
+            sub_ld_mat = full_ld_mat[rix_keep, rix_keep]
             input = MendelianRandomization::mr_mvinput(bx=bx, bxse=bxse, by=by, byse=byse, correlation=sub_ld_mat)
         } else {
             input = MendelianRandomization::mr_mvinput(bx=bx, bxse=bxse, by=by, byse=byse)
@@ -103,12 +106,14 @@ for (outcome_ix in 1:num_trait) {
             effects = c(effects, res$Estimate[mvivw_exp_ix])
             ps = c(ps, res$Pvalue[mvivw_exp_ix])
             sk_adj = c(sk_adj, pxp_adj[exposure_ix, outcome_ix] == 1)
+            num_snps = c(num_snp, dim(bx)[1])
         } else {
             sources = c(sources, exposure_ix)
             sinks = c(sinks, outcome_ix)
             effects = c(effects, 0.0)
             ps = c(ps, 1.0)
             sk_adj = c(sk_adj, pxp_adj[exposure_ix, outcome_ix] == 1)
+            num_snps = c(num_snp, dim(bx)[1])
         }
     }
 }
@@ -118,7 +123,8 @@ output = data.frame(
     sink=sinks,
     effect=effects,
     p=ps,
-    sk_adj=sk_adj
+    sk_adj=sk_adj,
+    num_snps=num_snps
 )
 
 write.table(output, file=output_file, sep="\t", quote=FALSE, row.names=FALSE)
